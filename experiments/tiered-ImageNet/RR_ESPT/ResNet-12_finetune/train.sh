eab576318290e0eb9a5fc56f1b325b4457c7709c
Yi Rong <54604983+Whut-YiRong@users.noreply.github.com>
2023-03-14 10:10:07 +0800

    Create train.sh

---
+++ experiments/tiered-ImageNet/RR_ESPT/ResNet-12_finetune/train.sh
@@ -0,0 +1,24 @@
+
+python train.py \
+    --opt sgd \
+    --lr 1e-3 \
+    --gamma 1e-1 \
+    --epoch 450 \
+    --epoch_size 100 \
+    --decay_epoch 50 250 450 \
+    --val_epoch 10 \
+    --weight_decay 5e-4 \
+    --nesterov \
+    --pre \
+    --resnet \
+    --train_way 5 \
+    --train_support_shot 5 \
+    --train_query_shot 15 \
+    --train_transform_type 0 \
+    --test_way 5 \
+    --test_support_shot 1 5 \
+    --test_query_shot 16 \
+    --alpha 200.0 \
+    --weight 0.03 \
+    --seed 1 \
+    --gpu 0

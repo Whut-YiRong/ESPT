f86a42514401a0f6403cd748ab8d5b4632065a16
Yi Rong <54604983+Whut-YiRong@users.noreply.github.com>
2023-03-14 09:56:31 +0800

    Add files via upload

---
+++ experiments/mini-ImageNet/ResNet-12_pretrain/ESPT/train.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+python train.py \
+     --opt sgd \
+     --lr 1e-1 \
+     --gamma 1e-1 \
+     --epoch 350 \
+     --decay_epoch 200 300 \
+     --batch_size 128 \
+     --val_epoch 25 \
+     --weight_decay 5e-4 \
+     --nesterov \
+     --pre \
+     --resnet \
+     --train_way 5 \
+     --train_support_shot 5 \
+     --train_query_shot 15 \
+     --train_transform_type 0 \
+     --test_way 5 \
+     --test_support_shot 1 5 \
+     --test_query_shot 16 \
+     --alpha 1.0 \
+     --weight 0.0 \
+     --seed 2 \
+     --gpu 0
